3527f442dd278bdae9865d1d35e11436cf95bb23
Samuel Nguyen <lesamuengu@gmail.com>
2023-04-24 23:45:25 -0700

    Basic login and registration implemented

--- app.py
+++ app.py
@@ -1,9 +1,88 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, abort
 import database as db
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 
 # Initialize Flask app
 app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
 app.secret_key = '0v5hY7S4Ki'
+
+# Login Manager (FlaskLogin)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.getUser(user_id)
+
+def redirect_login():
+    return redirect(url_for('login_page'))
+login_manager.unauthorized_handler(redirect_login)
+
+# Testing pages
+@app.route('/')
+@login_required
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login/', methods=['GET'])
+@app.route('/login', methods=['GET'])
+def login_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    player = db.login(username, password)
+
+    if player:
+        login_user(player)
+        return redirect(url_for('home_page'))
+    
+    return redirect(url_for('login_page'))
+    
+@app.route('/logout/')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login_page'))
+
+@app.route('/register/', methods=['GET'])
+@app.route('/register', methods=['GET'])
+def register_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    player = db.register(username, password)
+
+    if player:
+        login_user(player)
+        return redirect(url_for('home_page'))
+    return "Username is taken!", 409
+
+# REMOVE THIS FROM FINAL PRODUCT
+@app.route('/reset/')
+@app.route('/reset')
+def reset():
+    db.reset()
+    return redirect(url_for('home_page'))
 
 if __name__ == '__main__':
     db.init(app)
--- database.py
+++ database.py
@@ -4,12 +4,83 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+class Player(UserMixin, db.Model):
+    """
+    The user object.
+
+    Attributes:
+     - id -- internal database unique identifier
+     - username -- unique identifier used to log in
+     - password -- encrypted and salted, used to log in
+
+    Methods:
+     - get_id() -- return id
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def get_id(self):
+        return str(self.id)
+    
+
+def getUser(id: int) -> Player:
+    """
+    Retrieves a Player by ID
+
+    Arguments:
+     - id -- id of Player object
+    """
+
+    return db.session.execute(db.select(Player).where(Player.id == id)).scalar()
+
+def login(username: str, password: str) -> Player:
+    """
+    Retrieves a Player by username and password, if any; if none, returns None.
+
+    Arguments:
+     - username -- username of Player
+     - password -- password of Player
+    """
+
+    return db.session.execute(db.select(Player).where(Player.username == username, Player.password == password)).scalar()
+
+def register(username: str, password: str) -> Player:
+    """
+    Tries to create a new user account.
+    If successful, returns created account.
+    If user with same username already exists, then returns None
+
+    Arguments:
+     - username -- username of new Player
+     - password -- password of new Player
+    """
+    
+    # Create new user
+    try:
+        newUser = db.session.execute(db.insert(Player).values(username = username, password = password).returning(Player)).scalar()
+        db.session.commit()
+    except db.exc.IntegrityError:
+        # If username already taken
+        return None
+    
+    return newUser
+    
+def reset() -> None:
+    """
+    Deletes and recreates the database.
+    """
+    
+    db.drop_all()
+    db.create_all()
+
 def init(app: Flask) -> None:
     """
     Attaches the database to the current session. Call in main script before running the Flask application.
 
     Arguments:
-        app -- the Flask app
+     - app -- the Flask app
     """
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
