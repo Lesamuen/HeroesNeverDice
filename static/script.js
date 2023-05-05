6c8ce4476ee9627a4688b0eb45b2b85ba7a62148
Shalyn Nguyen <ngshalyn@gmail.com>
2023-05-05 14:56:23 -0700

    edit style for tables and colors. create script.js

---
+++ static/script.js
@@ -0,0 +1,27 @@
+function sellItem() {
+    // Sell Item in market place 
+    // items are found in the inventory list
+
+}
+
+function buyItem() {
+    // Buy Item in market place 
+    // can only buy items that are in the list of items for sale
+    // items will appear in the list with a buy button next to them
+    // need to get function to display list of items in market
+
+}
+
+function equipItem() {
+    // Equip Item
+    // items are found in the inventory list
+    // items will appear in the list with an equip button next to them
+    // if equip button is clicked, item will be equipped and stats will be updated into Equipped Items list
+}
+
+function unequipItem() {
+    // Unequip Item
+    // items are found in the equipped items list
+    // items will appear in the list with an unequip button next to them
+    // if unequip button is clicked, item will be unequipped and stats will be updated into Inventory list
+}
